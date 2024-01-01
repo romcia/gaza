203969db18a7be2adbf0aeb6fff0b4140b7d837c
Romcia <99322129+romcia@users.noreply.github.com>
2024-01-01 16:23:57 +0000

    Update process.py

--- process.py
+++ process.py
@@ -1,5 +1,12 @@
 import sys
-print("<h1> Data on killed and injured Palestinians in Gaza since 07/10/23 </h1>")
+import datetime
+now = datetime.now()
+title = "Data on killed and injured Palestinians in Gaza since 07/10/23"
+subtitle = f"Data captured on {now}"
+
+print(f"<h1>{title}</h1>")
+print(f"<h2>{subtitle}</h2")
+
 print("<pre>")
 for line in sys.stdin:
   print(line)
