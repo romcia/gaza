7c289df9491e64a57db2f84acadd98df306eaa50
Romcia <99322129+romcia@users.noreply.github.com>
2024-01-01 16:28:09 +0000

    Update process.py

--- process.py
+++ process.py
@@ -5,7 +5,7 @@ title = "Data on killed and injured Palestinians in Gaza since 07/10/23"
 subtitle = f"Data captured on {now}"
 
 print(f"<h1>{title}</h1>")
-print(f"<h2>{subtitle}</h2")
+print(f"<h2>{subtitle}</h2>")
 
 print("<pre>")
 for line in sys.stdin:
