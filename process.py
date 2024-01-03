68ead01c8d84f2af525c56a7dfd61efafce3ffc8
Romi Sobecka <romciamaria@gmail.com>
2024-01-03 14:16:58 +0000

    use python with and reformat

--- process.py
+++ process.py
@@ -1,22 +1,24 @@
 import pandas as pd
 import datetime
 
+
 def main():
     now = datetime.datetime.now()
     title = "Data on killed and injured Palestinians in Gaza since 07/10/23"
     subtitle = f"Data captured on {now}"
 
-    print(f"<h1>{title}</h1>")
-    print(f"<h2>{subtitle}</h2>")
-
     try:
         # Read the CSV file into a DataFrame
         data = pd.read_csv("data.csv")
-        f = open("index.html", "w")
-        f.write(data.to_html(index=False)) 
-        f.close()
+        with open(
+            "index.html", "w"
+        ) as f:  # with avoids needing to explicitly close file
+            f.write(f"<h1>{title}</h1>")
+            f.write(f"<h2>{subtitle}</h2>")
+            f.write(data.to_html(index=False))
     except Exception as e:
         print(f"Error reading data: {e}")
+
 
 if __name__ == "__main__":
     main()
