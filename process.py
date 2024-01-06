60f6f1abfec15ea696c5fe2f67028e34261fffef
Romcia <99322129+romcia@users.noreply.github.com>
2024-01-06 21:16:33 +0000

    Update process.py

--- process.py
+++ process.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import datetime
 
-
 def main():
     now = datetime.datetime.now()
     title = "Data on killed and injured Palestinians in Gaza since 07/10/23"
@@ -10,15 +9,17 @@ def main():
     try:
         # Read the CSV file into a DataFrame
         data = pd.read_csv("data.csv")
-        with open(
-            "index.html", "w"
-        ) as f:  # with avoids needing to explicitly close file
+        with open("index.html", "w") as f:
             f.write(f"<h1>{title}</h1>\n")
             f.write(f"<h2>{subtitle}</h2>\n")
+            # Convert DataFrame to HTML and write it to file
             f.write(data.to_html(index=False))
+            # Write explanatory text after the table
+            f.write("<p>*Source: Ministry of Health in Gaza</p>\n")
+            f.write("<p>Please note that any gaps or missing numbers are a result of unavailable corresponding data</p>\n")
+            f.write("<p>Following the collapse of services and communications at hospitals in the North, the MoH in Gaza did not update casualty figures</p>\n")
     except Exception as e:
         print(f"Error reading data: {e}")
-
 
 if __name__ == "__main__":
     main()
